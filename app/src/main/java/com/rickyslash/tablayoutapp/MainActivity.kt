8e1f8cc76668c2df6071005963122fe27b250fe6
ricky-kiva <ricky.kiva@gmail.com>
2023-03-19 19:32:47 +0700

    implement TabLayout 1 fragment

--- app/src/main/java/com/rickyslash/tablayoutapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/tablayoutapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.rickyslash.tablayoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    // this for companion object is for 'tab title'
+    companion object {
+        // '@StringRes' will get the 'resource' from 'string.xml'
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // this will instantiate the Adapter, and the 'rest is finding the View'
+        // this also 'passing adapter' to 'viewPager'
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+
+        // this will 'link' the 'LayoutTab' & the 'ViewPager'
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            // this will 'set title for the tab'
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        // this set 'elevation' (such 3D shadow)
+        supportActionBar?.elevation = 0f
+
     }
 }
---
+++ app/src/main/java/com/rickyslash/tablayoutapp/SectionsPagerAdapter.kt
@@ -0,0 +1,42 @@
+package com.rickyslash.tablayoutapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+// FragmentStateAdapter is a Jetpack Library 'class' that 'provide contents' for 'ViewPager'
+// 'activity: AppCompatActivity' used to 'pass reference of the activity' 'to the adapter', so the 'Adapter could access' it's 'context' / 'resource'
+// if Fragment is used, change constructor type to 'FragmentActivity'
+class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+
+    /* this not used because we are going to make things more flexible
+
+    // this will 'display assigned fragment' 'based on position'
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+    }*/
+
+    // createFragment() will be 'executed 3 times', because 'getItemCount() return 3'
+    // for each iteration, 'position' will be 'iterated'
+    override fun createFragment(position: Int): Fragment {
+        val fragment = HomeFragment()
+        // this will bundle the 'position' to the 'arguments'
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position+1)
+        }
+        return fragment
+    }
+
+    // this will tell 'how much tab to be displayed'
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+
+}
